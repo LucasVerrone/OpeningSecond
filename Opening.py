3e4d27ba46ab85346fc9e57edde57a2f7d9256e2
Lucas Verrone <101367035+LucasVerrone@users.noreply.github.com>
2025-01-10 20:55:32 -0300

    Open daily apps

---
+++ Opening.py
@@ -0,0 +1,34 @@
+import pyautogui
+
+pyautogui.PAUSE = 3.0
+pyautogui.PAUSE = True
+
+# abre o firefox #
+pyautogui.hotkey('win')
+pyautogui.write('firefox')
+pyautogui.hotkey('enter')
+
+# abre o brave #
+pyautogui.hotkey('win')
+pyautogui.write('brave')
+pyautogui.hotkey('enter')
+
+# abre o VScode #
+pyautogui.hotkey('win')
+pyautogui.write('code')
+pyautogui.hotkey('enter')
+
+# abre o pyCharm #
+pyautogui.hotkey('win')
+pyautogui.write('pycharm')
+pyautogui.hotkey('enter')
+
+# abre o whatsapp #
+pyautogui.hotkey('win')
+pyautogui.write('whatsapp')
+pyautogui.hotkey('enter')
+
+# abre o IntelliJ #
+pyautogui.hotkey('win')
+pyautogui.write('intellij')
+pyautogui.hotkey('enter')
